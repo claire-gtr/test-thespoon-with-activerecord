85bfb84928b8e9c1658fd79e1413caa2072296fd
claire gautier <claire.gautier@edhec.com>
2021-02-15 20:19:00 +0100

    finished test lecture

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #verb '/url', to: "controller#action"
+
+  # # see all the restaurants
+  # get '/restaurants', to: "restaurants#index"
+
+  # # create a restaurant :
+  #   # display the form
+  # get '/restaurants/new', to: "restaurants#new", as: :new_restaurant
+  #   # post information to DB
+  # post '/restaurants', to: "restaurants#create"
+
+  # # see one restaurant
+  # get '/restaurants/:id', to: "restaurants#show"
+
+  # # update a restaurant :
+  #   # display the form
+  # get '/restaurants/:id/edit', to: "restaurants#edit"
+  #   # post the new information to the DB
+  # patch '/restaurants/:id', to: "restaurants#update"
+
+  # # destroy a restaurant
+  # delete '/restaurants/:id', to: "restaurants#destroy"
+  resources :restaurants
 end
